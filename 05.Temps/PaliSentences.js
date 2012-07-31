0f0b7bfd85f03fcfaa9f83c8e22d6af598dec7ee
anhvurz90 <vuna@exoplatform.com>
2012-07-31 18:28:08 +0700

    add more data to temp

---
+++ 05.Temps/PaliSentences.js
@@ -0,0 +1,207 @@
+31/07/2012: Ratti (f) ban đêm
+
+29/07/2012: Uttarāsaṅga (m) phần y thượng, phần y dùng mặc ở trên
+
+28/07/2012: Nivāsana (nt) phần hạ y, y nội
+
+27/07/2012: Kañcuka (m) áo khoác ngoài, áo choàng
+
+26/07/2012: Veṭhana (nt) khăn đóng, khăn hay nón dùng đội trên đầu.
+
+25/07/2012: Dhāreti (v) cầm, mang (vật nào đó); mặc (quần áo); thoa (dầu thơm, nước hoa)
+
+24/07/2012: Candana (nt) gỗ trầm hương; (dầu thơm) có mùi trầm hương
+
+23/07/2012: Pupphati (v) trổ hoa, nở hoa
+
+22/07/2012: Attha (m) sự lợi ích; của cải.
+
+21/07/2012: Puṇḍarīka (nt) hoa sen đỏ
+
+20/07/2012: Paduma (nt) hoa sen trắng
+
+19/07/2012: Uppala (nt) bông súng ( màu xanh)
+
+18/07/2012: Nivesana (nt) nơi ở, nhà; lối vào
+
+17/07/2012: Hoti (v) là; có
+
+16/07/2012: Karoti (v) làm; xây dựng
+
+15/07/2012: Pokkharaṇī (f) cái hồ, cái ao; cái hồ nhân tạo
+
+14/07/2012: Pitu (m) người cha, ba, bố...
+
+13/07/2012: Sukhumāla (adj) (tả về vóc dáng) thanh lịch; được sự nuôi dưỡng tốt
+
+12/07/2012: Bhikkhu (m) vị tỳ khưu.
+
+11/07/2012: Amha (pron.) tôi, chúng tôi.
+
+08/07/2012: Sadā (adv) luôn luôn
+
+07/07/2012: Buddharūpa (nt) hình Phật, tượng Phật
+
+06/07/2012: Bodhi (f) sự giác ngộ; cây bồ đề
+
+05/07/2012: Sarīradhātu (f ) xá - lợi thân
+
+04/07/2012: Ṭhāna (nt) nơi chốn, chỗ, vị trí.
+
+03/07/2012: Sabba (adj) tất cả, mọi (thứ), mọi (người), v.v...
+
+02/07/2012: Cetiya (nt) tháp thờ, bảo tháp
+
+01/07/2012: Vandati (v) đảnh lễ, lễ bái
+
+30/06/2012: Magga (m) con đường; phương cách, cách thức; đạo (trong đạo quả chứng đạt)
+
+29/06/2012: Atthi (v) có được, đạt được
+
+28/06/2012: Khanti (f) sự nhẫn nại, sự kiên nhẫn
+
+27/06/2012: Bhuñjati (v) ăn, thọ thực, hưởng thụ (trong vật thực)
+
+26/06/2012: Vimutti (f ) sự thoát khỏi, sự giải thoát.
+
+25/06/2012: gacchati (v) đi, đi đến.
+
+24/06/2012: Viriya (nt) tinh tấn, cố gắng, nỗ lực
+
+23/06/2012: Deva (m) chư Thiên; “bầu trời”, “mây mưa”; “nhà vua”
+
+22/06/2012: Sīla (nt) tự nhiên, thói quen, giới hạnh, điều luật
+
+21/06/2012: Dāna (nt) sự cho, sự bố thí, vật cho, món quà
+
+20/06/2012: Virati ( f ) sự kiêng cử, sự tránh xa.
+
+19/06/2012: Kamma (nt) hành động; hành vi; công việc; việc làm, nghiệp
+
+18/06/2012: Kāya (m) một đống, một bộ sưu tập, cơ thể (thân)
+
+17/06/2012: Citta nt. tâm, tinh thần, tư tưởng.
+
+ 
+
+------------------------------
+
+Ven Adhiṭṭhāna và Ven Tieu Lam phụ trách
+
+ĐỀ NGHỊ: MỖI NGÀY HỌC MỘT TỪ PĀLI:
+
+ 
+
+Mục đích: Giúp cho những thành viên của Group có những vốn từ Pāli căn bản.
+
+Cần sự góp ý thêm của mọi người để việc này được sinh động và gặt hái kết quả tốt.
+
+Việc này xin nhờ hai vị Ven Adhiṭṭhāna và Ven Tieu Lam phụ trách giùm.
+
+ 
+
+------------------------------
+
+TỪ VIẾT TẮT - ABBREVIATION.
+
+n. = noun (danh từ, P: nāmanāma);
+
+m. = masculine (nam tánh);
+
+f. = feminine (nữ tánh);
+
+nt. = neuter (trung tánh);
+
+adj. = adjective (tính từ, P: guṇanāma);
+
+pron. = pronoun (đại từ, P: sabbanāma)
+
+v. = verb (động từ);
+
+adv. = adverb (trạng từ);
+
+ger. = gerund (danh động từ);
+
+pref. = prefix (tiếp đầu ngữ);
+
+suf. = suffix (tiếp vĩ ngữ);
+
+conj. = conjunction (liên từ);
+
+indecl. = indeclinable (từ nối);
+
+pass. = passive (thụ động cách);
+
+ppp. = perfect passive participle (phân từ);
+
+dat. = dative (chỉ định cách);
+
+instr. = instrumentive (sử dụng cách);
+
+gen. = genitive (thuộc cách);
+
+loc. = locative (định sở cách);
+
+voc. = vocative (hô cách);
+
+...
+
+
+
+1 Ven Adhiṭṭhāna Kết lại câu thứ nhất vừa học :
+Sukhumālo ahaṃ bhikkhave paramasukhumālo accanatakhumālo.
+Trong câu này các chữ như sukhumālo, paramasukhumālo, accantasukhumālo ở dưới dạng tính từ do đó bổ nghĩa cho chủ từ là chữ ahaṃ.
+Này các thầy tỳ khưu ! Ta được sự nuôi dưỡng tốt, được sự nuôi dưỡng tốt nhất, được sự nuôi dưỡng một cách tột bật
+ngày 14 tháng 7 lúc 9:09 sáng · Thích · 2
+Ven Adhiṭṭhāna Trong chú giải ghi như sau:
+Imaṃ bhagavā kapilapure nibbattakālato paṭṭhāya niddukkhabhāvaṃ gahetvā āha, cariyakāle pana tena anubhūtadukkhassa anto natthīti
+Đức Thế Tôn từ lúc sinh ra tại nước Kapila đã sống trong tình trạng không có khổ, vì vậy mà không có sự trải nghiệm sự khổ trong đời ( chữ cariyakāle ý muốn nói đến khi Đức Thế Tôn còn là 1vị hoàng tử)
+
+Sukhumāla (adj) (tả về vóc dáng) thanh lịch; được sự nuôi dưỡng tốt
+- Với những từ tương tự có ý nghĩa mạnh hơn như:
+Paramasukhumāla (adj) thanh lịch nhất; được sự nuôi dưỡng tốt nhất
+Accantasukhumāla (adj) thanh lịch tột bậc, được sự nuôi dưỡng một cách tột bậc
+Thích · · Theo dõi · ngày 13 tháng 7 lúc 9:08 sáng
+
+    Hiếu Nguyễn và 6 người khác thích điều này.
+        Ven Adhiṭṭhāna sukhumāla(adj) là 1 từ khó chọn nghĩa nó bao gồm 3 ý nghĩa : đẹp, sang trọng, quý phái vì từ nhỏ đã được sống trong nhung lụa...
+        ngày 13 tháng 7 lúc 9:10 sáng · Đã được chỉnh sửa · Thích · 2
+
+
+2 
+Pitu (m) người cha, ba, bố..
+Pokkharaṇī (f) cái hồ, cái ao; cái hồ nhân tạo
+        Ven Adhiṭṭhāna không biết có phải là chữ pokkharaṇī này thường được nói đến hồ sen hay không, vì chữ pokkharaṇī được hình thành từ chữ pokkhara (nt) hoa sen + iṇī (tiếp vị ngữ) >> hình thành nên chữ pokkharaṇī.
+Nivesana (nt) nơi ở, nhà
+
+Kết lại đoạn văn thứ hai:
+Mama sudaṃ bhikkhave pitunivesane pokkharaṇiyo kāritā honti.
+"Này các thầy tỳ khưu ! Có nhiều hồ (sen) được xây nên trong nhà của cha ta."
+Phân tích :
+- mama - xuất phát từ chữ amha, trong ngữ cảnh của đoạn này, nó được chia theo sở thuộc cách số ít
+- pokkharaṇiyo là chủ cách số nhiều của chữ pokkharaṇī
+- kārita bổ nghĩa cho chữ pokkharaṇī nên biến thành kāritā
+- pitunivesananti pituno nivesanaṃ - nhà của cha; pitunivesane - tại, trong nhà của cha
+- honti bị biến thành số nhiều, từ chữ hoti.
+- sudaṃ là 1 dạng từ không được dịch (sẽ bắt gặp nhiều trong các văn bản khác).
+
+3 Kết lại đoạn thứ ba:
+Ekattha sudaṃ uppalaṃ pupphati ekattha padumaṃ ekattha puṇḍarīkaṃ, yāvadeva mamatthāya.
+Một hồ nở toàn sen xanh, một hồ nở toàn sen trắng, một hồ nở toàn sen hồng, chỉ dành cho (lợi ích) của ta.
+
+- Ekattha - một nơi, ở một nơi. trong trường hợp này chữ ekatthāti ekissā pokkharaṇiyā - mang nghĩa là tại một hồ sen.
+- Yāvadeva = yāva + eva - chỉ dành cho.
+- mamatthāya = mama + atthāya = cho lợi ích của ta.
+
+4 Tổng kết đoạn thứ tư
+Na kho panāhaṃ bhikkhave akāsikaṃ candanaṃ dhāremi, kāsikaṃ su me taṃ bhikkhave veṭhanaṃ hoti, kāsikā kañcukā, kāsikaṃ nivāsanaṃ, kāsiko uttarasaṅgo.
+Này các thầy tỳ khưu ! (Ta) không thoa dầu thơm có mùi trầm hương mà không thuộc xứ Kāsī, này các thầy tỳ khưu ! các khăn đống của ta thuộc xứ Kāsī, những cái áo choàng (của ta) thuộc xứ Kāsī, nội y (của ta) thuộc xứ Kāsī, thượng y (của ta) thuộc xứ Kāsī.
+
+5 Ratti (f) ban đêm
+
+Câu thứ năm sẽ học : Rattindivaṃ kho panassu me taṃ bhikkhave setacchattaṃ dhārīyati, mā maṃ phusi sītaṃ vā uṇhaṃ vā rajo vā tiṇaṃ vā ussāvo vāti.
+
+
+
+
+
